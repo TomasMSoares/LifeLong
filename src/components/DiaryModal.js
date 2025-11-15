7253336063f29d14ef73d6cba713c38a86bf0b76
ZeAfonso21 <joseafonsogat@gmail.com>
2025-11-15 22:28:36 +0100

    post it

--- src/components/DiaryModal.js
+++ src/components/DiaryModal.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
+function randomPercent() {
+  return Math.floor(Math.random() * (90 - 10 + 1)) + 10;
+}
+
 export default function DiaryModal({ paragraphs, images }) {
   return (
     <div
       className="
-        bg-[repeating-linear-gradient(#d6d6d6_0px,#d6d6d6_1px,transparent_1px,transparent_24px)]
-        bg-[url('/notebook-paper-texture.png')]
+        flex flex-col
+        items-center
+        gap-10
+        bg-white
         px-4
         py-6
       "
@@ -15,11 +21,15 @@ export default function DiaryModal({ paragraphs, images }) {
           (file) => Number(images[file]) === index
         );
         const firstImage = imageFiles[0] ?? null;
+        const random = randomPercent();
+        const left_random = `left-[${random}%]`;
+        console.log(left_random);
 
         return (
           <div
             key={index}
             className="
+
               max-w-xl
               mx-auto
               mb-6               /* ≈ 24px, keeps blocks aligned to lines */
@@ -29,20 +39,31 @@ export default function DiaryModal({ paragraphs, images }) {
             "
           >
             {/* Paragraph text */}
-            <p className="font-serif"
+            {/* <p className="font-serif"
             style={{ fontFamily: "var(--font-quicksand)" }}
             >
               {text}
-            </p>
+            </p> */}
 
             {/* Image */}
             {firstImage && (
-              <div className="mt-3">
+              <div className="mt-3 relative ">
                 <img
                   src={`/${firstImage}`}
                   alt={`Paragraph ${index} image`}
                   className="w-full rounded-md block mb-10"
                 />
+                <div className={`absolute top-[80%] ${left_random} drop-shadow-xl`}>
+                    <img 
+                    className=' absolute top-0 w-32 h-32'
+                    src='/post-it.png'
+                    />
+                    <div
+                    className=' mt-5 -rotate-4  w-32 h-32'
+                    >
+                        <span>Hello</span>
+                    </div>
+                </div>
               </div>
             )}
           </div>
