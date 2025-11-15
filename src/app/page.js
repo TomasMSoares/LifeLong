85a39108857a958cc02d45525f99aac65ce2824c
ZeAfonso21 <joseafonsogat@gmail.com>
2025-11-15 17:06:18 +0100

    add initial state

--- src/app/page.js
+++ src/app/page.js
@@ -7,6 +7,7 @@ import EntryDetailModal from '@/components/EntryDetailModal';
 import { Button } from '@/components/ui/button';
 import { loadEntries, saveEntry } from '@/lib/storage';
 import AppHeader from '@/components/AppHeader';
+import InitialPage from '@/components/InitialPage';
 
 export default function Home() {
   const [entries, setEntries] = useState([]);
@@ -27,6 +28,11 @@ export default function Home() {
   return (
     <div className="h-screen bg-gradient-to-b from-white to-warmBeige flex-col">
       <AppHeader/>
+      <InitialPage/>
+      {/* <MainContent/> */}
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </div>
     </div>
   );
 }
